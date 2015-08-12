ea860a52459f76811e6ff5bcd984567b7e2a7d41
Amber Willett <aelise+github@gmail.com>
2015-08-11 22:50:51 -0400

    Initial commit - CodeKata from http://www.codingdojo.org/cgi-bin/index.pl?KataBankOCR

---
+++ katabank.py
@@ -0,0 +1,63 @@
+import sys
+
+
+def rework(threelines):
+  """ """
+  #Takes an array of three lines, X characters and reworks to 
+  #an array of X lines, 12 characters each
+  #(each 12-char line in the output consists of 3 characters from 
+  #each row, appended with those above/below them)
+  #assumes each line ends with carriage return (\n)
+  result = []
+  temp = range(len(threelines[0])-2)
+  for i,y in enumerate(temp[0::3]):
+    result.append([])
+    for x in range(3):
+      result[i].extend(threelines[x][y:y+3])
+  return [''.join(result[i]) for i,x in enumerate(result)]
+
+
+def numberlookup(codes,numlookupdict):
+  """ """
+  #
+
+
+
+def fileparser(filename,iskeyortest,numlookupdict):
+  """ Loops through files four lines at a time and
+  takes appropriate action based on 'iskeyortest' param """
+  #
+
+  with open(filename) as myfile:
+    threelines = []
+    results = []
+    for i,line in enumerate(myfile):
+      if i % 4 == 3 and i>0:
+        n = rework(threelines)
+        if iskeyortest == 'key':
+          results = dict(zip(n,line)
+        elif iskeyortest == 'test':
+          results.append(numberlookup(n,numlookupdict))
+          threelines = []
+        else: 
+          print 'Error:Instructions unclear'
+      else:
+          threelines.append(line)
+  return results
+
+
+def main(keyfile, testfile):
+  """ """
+  #
+
+  #
+  numlookupdict = fileparser(keyfile, 'key',[])
+  
+  #
+  finalnums = fileparser(testfile, 'test', numlookupdict)
+
+  return finalnums 
+
+
+if __name__ == '__main__':
+    main(sys.argv[1],sys.argv[2])
