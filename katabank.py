0ff454491d3e0614d07cbc2f294838f4e18b6b05
Amber Willett <aelise+github@gmail.com>
2015-08-12 22:30:44 -0400

    Finished with Story 2 and 3, also added code to handle illegible characters '?'

--- katabank.py
+++ katabank.py
@@ -1,11 +1,15 @@
+"""
+
+"""
+
 import sys
 
 
-def rework(threelines):
-  """ """
-  #Takes an array of three lines, X characters and reworks to 
-  #an array of X lines, 12 characters each
-  #(each 12-char line in the output consists of 3 characters from 
+def rework3x3to1x9(threelines):
+  """ Takes an array of three lines, X*3 characters each and reworks  
+      to an array of X lines, 9 characters each
+  """
+  #(each 9-char line in the output consists of 3 characters from 
   #each row, appended with those above/below them)
   #assumes each line ends with carriage return (\n)
   result = []
@@ -17,26 +21,29 @@ def rework(threelines):
   return [''.join(result[i]) for i,x in enumerate(result)]
 
 
-def charlookup(codes,lookupdict):
-  """ """
+def charlookup(mykeys,lookupdict):
+  """ Simple dict lookup for a list of keys
+      Assumes values are strings and returns one aggregated string. 
+  """
   finalseq = ''
-  for x in codes:
-    finalseq += lookupdict[x]
+  for x in mykeys:
+    try:
+      finalseq += lookupdict[x]
+    except Exception, e:
+      finalseq += '?'
   return finalseq
-
 
 
 def fileparser(filename,iskeyortest,lookupdict):
   """ Loops through files four lines at a time and
-  takes appropriate action based on 'iskeyortest' param """
-  #
-
+      takes appropriate action based on 'iskeyortest' param 
+  """
   with open(filename) as myfile:
     threelines = []
     results = []
     for i,line in enumerate(myfile):
       if i % 4 == 3 and i>0:
-        n = rework(threelines)
+        n = rework3x3to1x9(threelines)
         if iskeyortest == 'key':
           results = dict(zip(n,line))
         elif iskeyortest == 'test':
@@ -49,16 +56,47 @@ def fileparser(filename,iskeyortest,lookupdict):
   return results
 
 
-def main(keyfile, testfile):
-  """ """
+def mychecksum(num):
+  """ Compares a 9-digit number to the following checksum:
+      checksum calculation:
+      (d1+2*d2+3*d3 +..+9*d9) mod 11 = 0
+      Returns boolean 
+  """
+  num = str(num)
+  check = 0
+  for i in range(9):
+    check += (i+1)*int(num[8-i])
 
-  #
+  if check%11==0:
+    return True
+  else:
+    return False
+
+
+def main(keyfile, testfile):
+  """ 
+  """
+  #Make lookup table for known characters
   numlookupdict = fileparser(keyfile, 'key',[])
   
-  #
+  #Proceed with actual test data to be parsed
   finalnums = fileparser(testfile, 'test', numlookupdict)
+  print finalnums
 
-  print finalnums 
+  #Checksum each num in finalnums and record pass/fail
+  for i,x in enumerate(finalnums):
+    if x.find('?')==-1:
+      if mychecksum(x)==False:
+        finalnums[i]=x+' ERR'
+    else:
+      finalnums[i]=x+' ILL'
+
+  print finalnums
+
+  #Check ERR/ILL values for missing _ or | (or label AMB)
+
+
+
 
 
 if __name__ == '__main__':
--- testingscript.py
+++ testingscript.py
@@ -4,7 +4,7 @@ test=[' _     _  _     _  _  _  _  _ \n',
       '| |  | _| _||_||_ |_   ||_||_|\n',
       '|_|  ||_  _|  | _||_|  ||_| _|\n'] 
 
-temp = katabank.rework(test)
+temp = katabank.rework3x3to1x9(test)
 temp==[' _ | ||_|',
  '     |  |',
  ' _  _||_ ',
@@ -16,8 +16,27 @@ temp==[' _ | ||_|',
  ' _ |_||_|',
  ' _ |_| _|']
 
-temp2 = katabank.fileparser('mykeyfile.txt','key',[])
+tempdict = katabank.fileparser('mykeyfile.txt','key',[])
+temp==tempdict
 
-temp==temp2
+katabank.charlookup(temp,tempdict)
 
-katabank.charlookup(' _ | ||_|',temp2)
+temp2=[' _ | ||_|',
+ '_    |  |',
+ ' _  _||_ ',
+ ' _  _| _|',
+ '   |_|  |',
+ ' _ |_  _|',
+ ' _  _ |_|',
+ ' _   |  |',
+ ' _ |_||_|',
+ ' _ |_| _|']
+
+katabank.charlookup(temp2,tempdict)
+
+wholetest = katabank.fileparser('testdata.txt','test',tempdict)
+
+run katabank 'mykeyfile.txt' 'testdata.txt'
+
+katabank.mychecksum('457508000')==True
+katabank.mychecksum('664371495')==False
